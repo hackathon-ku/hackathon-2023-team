9f1c1ab905bcc75be838abab1c85c4ac36d5641d
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-11-22 13:24:44 +0700

    Add post detail initial look :lipstick:

---
+++ src/app/clubs/posts/[id]/page.tsx
@@ -0,0 +1,27 @@
+import PostDetail from "@/components/PostDetail";
+import Image from "next/image";
+
+interface PostDetailPageProps {
+	params: { id: string };
+}
+
+export default async function PostDetailPage({ params }: PostDetailPageProps) {
+	return (
+		<div className="flex min-h-screen flex-col items-center bg-white">
+			<div className="h-fit w-full relative">
+				<Image
+					src={"/event.png"}
+					width={0}
+					height={0}
+					sizes="100vw"
+					style={{ width: "100%", height: "auto" }}
+					alt={"event"}
+				/>
+				<div className="absolute bottom-0 p-3 font-bold bg-[#006664] text-white rounded-t-lg">
+					ชมรมดนตรีสากลมหาวิทยาลัยเกษตรศาสตร์ (เค ยู แบนด์)
+				</div>
+			</div>
+			<PostDetail />
+		</div>
+	);
+}
