8b03cf60e794df4427c085d79ecc1bb4b5c44dad
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-11-22 12:09:17 +0700

    Add event page layout

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import authOptions from "../api/auth/[...nextauth]/options";
 import "@mantine/dates/styles.css";
+import CalendarWrapper from "@/components/CalendarWrapper";
+import News from "@/components/News";
 
 export default async function EventPage() {
 	const session = await getServerSession(authOptions);
@@ -8,7 +10,12 @@ export default async function EventPage() {
 	if (session)
 		return (
 			<main className="flex min-h-screen flex-col items-center p-12 bg-white gap-8">
-				{JSON.stringify(session)}
+				<h1 className="self-start text-2xl font-bold">ตารางอีเว้นท์และกิจกรรม</h1>
+				<CalendarWrapper />
+				<h1 className="self-start text-2xl font-bold">ข่าวสารจากชมรม</h1>
+				<News />
+				<News />
+				<button className="self-end rounded-full py-1.5 px-6 border border-[#006664] text-[#006664] text-sm">ข่าวสารทั้งหมด</button>
 			</main>
 		);
 	return <h1>Event Page</h1>;
