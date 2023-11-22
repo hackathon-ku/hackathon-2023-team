7546aaf6dab6c8d7b55531e4eaba4c718ce9b575
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-11-22 11:37:18 +0700

    Add auth provider

---
+++ src/app/api/[...nextauth]/options.ts
@@ -0,0 +1,46 @@
+import { AuthOptions, RequestInternal, User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import axios from "axios";
+
+const authOptions: AuthOptions = {
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			authorize: async function (
+				credentials: Record<"username" | "password", string> | undefined,
+				req: Pick<RequestInternal, "body" | "query" | "headers" | "method">,
+			): Promise<User | null> {
+				if (!(credentials?.username || credentials?.password)) {
+					return null;
+				}
+
+				const res = await axios.post(`${process.env.NEXTAUTH_URL}/api/auth/login`, {
+					username: credentials.username,
+					password: credentials.password,
+				});
+
+				return res.data;
+			},
+		}),
+	],
+	pages: {
+		signIn: "/signin",
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+	callbacks: {
+		jwt: async ({ token, user }) => {
+			user && (token.user = user);
+			return token;
+		},
+		session: async ({ session, token }) => {
+			session.user = token.user as User;
+			return session;
+		},
+	},
+};
+
+export default authOptions;
---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+import NextAuth, { Awaitable, RequestInternal, User } from "next-auth";
+import authOptions from "./options";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
