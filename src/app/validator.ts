cf5f473194d4c4fbd68afdea8b4f5c0c20429901
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-11-22 14:09:38 +0700

    Added validate create event schema

---
+++ src/app/validator.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const createEventSchema = z.object({
+  title: z.string().min(1).max(255),
+  content: z.string().min(1),
+  // bannerUrl: z.string().url(),
+  location: z.string().min(1).max(255),
+  startDate: z.string().transform((str) => new Date(str)),
+  endDate: z.string().transform((str) => new Date(str)),
+  startTime: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
+  endTime: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
+  clubId: z.number(),
+});
