24cce61d7578bb4a0f2f7aaa954890d043235792
Bhokin Watanapitak <zbeamz@hotmail.com>
2023-11-23 21:46:26 +0700

    Add mock register member

--- src/app/api/mock-data.ts
+++ src/app/api/mock-data.ts
@@ -15,3 +15,18 @@ export const mockingUserResponse = {
 		},
 	},
 };
+
+export const registerMemberData = {
+	data: {
+		user: {
+			name: "มิกกี้ เมาส์",
+			year: "4",
+			faculty: "วิศวกรรมศาสตร์",
+			department: "ซอฟต์แวร์",
+			email: "micky_emmyay@ku.th",
+			phone: "0XX-XXX-XXXX",
+			reason:
+				"สนใจในการทำกิจกรรมด้านนี้ มีความชอบในการเล่นดนตรีมาตั้งแต่ยังจำความไม่ได้ และเล่นกีตาร์เป็นตั้งแต่อยู่ในท้องแม่",
+		},
+	},
+};
