93085098cdaac4ed5141c0ed849266975924fbe6
Bhokin Watanapitak <zbeamz@hotmail.com>
2023-11-23 19:02:44 +0700

    Make approve post and event

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,15 +6,13 @@ interface TagProps {
 }
 
 const Tag = ({ tagName, color }: TagProps) => {
-  
-  return (
-    <div 
-      className={`flex w-fit justify-center py-[3px] px-[10px] rounded-full ${color} text-white font-light text-[12px]`} 
-      style={{ backgroundColor: color.slice(4, -1)}}
-    >
-        {tagName}
-    </div>
-  )
-}
+	return (
+		<div
+			className={`flex w-fit justify-center py-[3px] px-[10px] rounded-full ${color} text-white font-light text-[12px]`}
+		>
+			{tagName}
+		</div>
+	);
+};
 
 export default Tag;
