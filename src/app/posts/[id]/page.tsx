02c785e60f687f8ea4334938d6ef8f71ddde75a5
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-11-23 12:36:11 +0700

    Add carousel and comment

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -4,6 +4,9 @@ import { PostIncludeAll } from "@/types/post";
 import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { cache } from "react";
+import { Carousel } from "@mantine/carousel";
+import CarouselWrapper from "./_components/CarouselWrapper";
+import Comment from "./_components/Comment";
 
 interface PostDetailPageProps {
 	params: { id: string };
@@ -22,22 +25,18 @@ export default async function PostDetailPage({ params }: PostDetailPageProps) {
 	return (
 		<div className="flex min-h-screen flex-col items-center bg-white">
 			<div className="h-fit w-full relative">
-				<Image
-					src={"/event.png"}
-					width={0}
-					height={0}
-					sizes="100vw"
-					style={{ width: "100%", height: "auto" }}
-					alt={"event"}
-				/>
-				<div className="absolute w-full bottom-0 p-3 font-bold bg-[#006664] text-white rounded-t-xl">
+				<CarouselWrapper />
+				<div className="absolute w-full -bottom-5 p-3 font-bold bg-[#006664] text-white rounded-t-xl">
 					{post.club.label}
 				</div>
 			</div>
 			<PostDetail post={post} />
+			<div className="w-full px-8 flex flex-col gap-3">
 			{post.comments.map((c) => (
-				<span key={c.id}>{c.message}</span>
+				<Comment key={c.id} name={"Dan"} message={c.message} createdAt={c.createdAt} />
 			))}
+			</div>
+
 		</div>
 	);
 }
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,8 +6,9 @@ interface TagProps {
 }
 
 const Tag = ({ tagName, color }: TagProps) => {
+  console.log(color)
   return (
-    <div className={`flex w-fit justify-center py-1 px-2 rounded-full ${color} text-white font-light text-sm`}>
+    <div className={`flex w-fit justify-center py-1 px-2 rounded-full ${color} text-white font-light text-sm`} style={{ backgroundColor: color.slice(4, -1)}}>
         {tagName}
     </div>
   )
