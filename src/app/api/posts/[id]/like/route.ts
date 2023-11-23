46abf43b52fa534f8e6cc085b640cd03a843c9bf
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-11-23 15:18:51 +0700

    Fixed like button and api like

--- src/app/api/posts/[id]/like/route.ts
+++ src/app/api/posts/[id]/like/route.ts
@@ -3,8 +3,22 @@ import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import type { PostParams } from "../types";
+import { z } from "zod";
+
+const createLikeSchema = z.object({
+	type: z.enum(["event", "post"]),
+});
 
 export async function POST(request: NextRequest, { params }: PostParams) {
+	const body = await request.json();
+
+	const validator = createLikeSchema.safeParse(body);
+	if (!validator.success) {
+		return NextResponse.json("กรุณากรอกข้อมูลให้ถูกต้อง", { status: 400 });
+	}
+
+	const { type } = validator.data;
+
 	const session = await getServerSession(authOptions);
 	if (!session) {
 		return NextResponse.json({ error: "กรุณาเข้าสู่ระบบ" }, { status: 401 });
@@ -14,7 +28,8 @@ export async function POST(request: NextRequest, { params }: PostParams) {
 		const newLike = await prisma.like.create({
 			data: {
 				userId: session.user.id,
-				postId: parseInt(params.id),
+				postId: type === "post" ? parseInt(params.id) : undefined,
+				eventId: type === "event" ? parseInt(params.id) : undefined,
 			},
 		});
 		return NextResponse.json(newLike, { status: 201 });
@@ -23,22 +38,36 @@ export async function POST(request: NextRequest, { params }: PostParams) {
 	}
 }
 
-export async function DELETE(reqeust: NextRequest, { params }: PostParams) {
+export async function DELETE(request: NextRequest, { params }: PostParams) {
+	const type = request.nextUrl.searchParams.get("type");
+
 	const session = await getServerSession(authOptions);
 	if (!session) {
 		return NextResponse.json({ error: "กรุณาเข้าสู่ระบบ" }, { status: 401 });
 	}
 
-	const post = await prisma.post.findUnique({ where: { id: parseInt(params.id) }, include: { likes: true } });
-	if (!post) {
-		return NextResponse.json({ error: "ไม่พบโพสต์นี้" }, { status: 404 });
-	}
+	try {
+		let like;
 
-	const like = post.likes.find((like) => like.userId === session.user.id && like.postId === post.id);
-	if (!like) {
-		return NextResponse.json({ error: "ไม่พบการถูกใจนี้" }, { status: 404 });
-	}
+		if (type === "event") {
+			const event = await prisma.event.findUnique({ where: { id: parseInt(params.id) }, include: { likes: true } });
+			if (!event) {
+				return NextResponse.json({ error: "ไม่พบกิจกรรมนี้" }, { status: 404 });
+			}
 
-	const deleltedLike = await prisma.like.delete({ where: { id: like.id } });
-	return NextResponse.json(deleltedLike, { status: 200 });
+			like = event.likes.find((like) => like.userId === session.user.id && like.eventId === event.id);
+		} else {
+			const post = await prisma.post.findUnique({ where: { id: parseInt(params.id) }, include: { likes: true } });
+			if (!post) {
+				return NextResponse.json({ error: "ไม่พบโพสต์นี้" }, { status: 404 });
+			}
+
+			like = post.likes.find((like) => like.userId === session.user.id && like.postId === post.id);
+		}
+
+		const deleltedLike = await prisma.like.delete({ where: { id: like?.id } });
+		return NextResponse.json(deleltedLike, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ error: error }, { status: 500 });
+	}
 }
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,20 +1,56 @@
 "use client";
 
-"use client";
-
-import { HeartIcon as HeartIconOutline } from "@heroicons/react/24/outline";
-import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
+import { GoHeartFill } from "react-icons/go";
+import { GoHeart } from "react-icons/go";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { FiHeart } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa6";
 
 type LikeButtonProps = {
 	isLike: boolean;
 	like: () => void;
 	unlike: () => void;
+	postId: number;
+	type: "event" | "post";
 };
 
-export default function LikeButton({ isLike, like, unlike }: LikeButtonProps) {
-	if (isLike) {
-		return <HeartIconSolid className="h-5 w-5 cursor-pointer text-red-500" onClick={unlike} />;
+export default function LikeButton({ isLike, like, unlike, postId, type }: LikeButtonProps) {
+	const { status } = useSession();
+
+	async function handleClickLike() {
+		if (status !== "authenticated") {
+			alert("กรุณาเข้าสู่ระบบ");
+			return;
+		}
+
+		try {
+			await axios.post(`/api/posts/${postId}/like`, {
+				type,
+			});
+			like();
+		} catch (error) {
+			console.error("Post like failed: ", error);
+		}
 	}
 
-	return <HeartIconOutline className="h-5 w-5 cursor-pointer" onClick={like} />;
+	async function handleClickUnlike() {
+		if (status !== "authenticated") {
+			alert("กรุณาเข้าสู่ระบบ");
+			return;
+		}
+
+		try {
+			await axios.delete(`/api/posts/${postId}/like?type=${type}`);
+			unlike();
+		} catch (error) {
+			console.error("Unlike failed: ", error);
+		}
+	}
+
+	if (isLike) {
+		return <FaHeart className="h-5 w-5 cursor-pointer text-red-500" onClick={handleClickUnlike} />;
+	}
+
+	return <FiHeart className="h-5 w-5 cursor-pointer" onClick={handleClickLike} />;
 }
