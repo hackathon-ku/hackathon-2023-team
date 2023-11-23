3a6dec3f94c41a12acf89c635d26091b7e71c411
Bhokin Watanapitak <zbeamz@hotmail.com>
2023-11-23 16:43:58 +0700

    Approve in News components

--- src/app/clubs/[id]/posts/requested/page.tsx
+++ src/app/clubs/[id]/posts/requested/page.tsx
@@ -3,6 +3,12 @@ import { cache } from "react";
 import PostForm from "@/app/clubs/_components/PostForm";
 import Link from "next/link";
 import News from "@/app/_components/News";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/api/auth/[...nextauth]/options";
+import Modal from "@/components/CustomModal";
+import { Post, User } from "@prisma/client";
+import CustomModal from "@/components/CustomModal";
+import { useDisclosure } from "@mantine/hooks";
 
 interface RequestedPostsPageProps {
 	params: { id: string };
@@ -15,9 +21,17 @@ const fetchNotApprovedPost = cache((clubId: number) =>
 	}),
 );
 
-export default async function requestedPostsPage({ params }: RequestedPostsPageProps) {
+const fetchMemberByUserId = cache((userId: number) =>
+	prisma.member.findUnique({
+		where: { userId: userId },
+	}),
+);
+
+export default async function RequestedPostsPage({ params }: RequestedPostsPageProps) {
 	const posts = await fetchNotApprovedPost(parseInt(params.id));
 	const club = await prisma.club.findUnique({ where: { id: parseInt(params.id) } });
+	const session = await getServerSession(authOptions);
+	const member = session ? await fetchMemberByUserId(session.user.id) : undefined;
 
 	if (!club) return null;
 
@@ -30,9 +44,9 @@ export default async function requestedPostsPage({ params }: RequestedPostsPageP
 				<div className="flex justify-between w-full"></div>
 				{posts
 					? posts.map((p) => (
-							<Link href={`/clubs/posts/${p.id}`} key={p.id}>
-								<News post={p} />
-							</Link>
+							<div key={p.id}>
+								<News post={p} role={member?.role} />
+							</div>
 					  ))
 					: "No waiting requested posts"}
 			</div>
